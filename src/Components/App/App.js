2b42c065afa6fbf9919513c470b9ee8e91afcf58
Kyle Robertson <91642555+KyleCRobertson@users.noreply.github.com>
2023-02-22 21:01:56 -0500

    Mark Belford website initial commit

---
+++ src/Components/App/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+      </header>
+      <section className='content'>
+        <h1>Mark Belford</h1>
+        <p>Surrounded by the red carpet glamour of the 2000 Academy Awards, fashion star Mark Belford was born. Since his Oscar debut with four custom commissioned gowns making their appearance, Mark has continued his focus on creating extraordinary couture attire for royalty, socialites and
+elebrities around the world.</p>
+      </section>
+    </div>
+  );
+}
+
+export default App;
