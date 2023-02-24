a81300776e507ac0b663f15251474abc306e8e6e
Kyle Robertson <91642555+KyleCRobertson@users.noreply.github.com>
2023-02-23 21:21:15 -0500

    Text addition and style changes

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -10,7 +10,10 @@ function App() {
         <p>Surrounded by the red carpet glamour of the 2000 Academy Awards, fashion star Mark Belford was born. Since his Oscar debut with four custom commissioned gowns making their appearance, Mark has continued his focus on creating extraordinary couture attire for royalty, socialites and
 elebrities around the world.<br/>
         <br/>
-        Immediately striking and ultimately timeless, every Mark Belford design is the ultimate expression of a client’s inner beauty, dressed with the grace, confidence and allure befitting that special occasion. His work is the product of his clients’ innermost couture desires – they are his muse; he the creative vessel that brings them to life.</p>
+        Immediately striking and ultimately timeless, every Mark Belford design is the ultimate expression of a client’s inner beauty, dressed with the grace, confidence and allure befitting that special occasion. His work is the product of his clients’ innermost couture desires – they are his muse; he the creative vessel that brings them to life.<br/>
+        <br/>
+        <h3>Toronto. New York. Los Angeles.</h3>
+        </p>
       </section>
     </div>
   );
